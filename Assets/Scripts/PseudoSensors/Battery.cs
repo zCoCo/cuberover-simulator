d1e181331b917709211bbab1fec03eb1449b1cc2
TheSchlueb <54086858+TheSchlueb@users.noreply.github.com>
2020-09-03 12:10:44 -0400

    sensors
    battery, rng, moon terrain

---
+++ Assets/Scripts/PseudoSensors/Battery.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Battery : MonoBehaviour
+{
+
+    private float startTime;
+    public Transform bar;
+    public float battery = 100;
+    public float rate = 10;
+    public int seconds;
+    private bool wait = false;
+    public Text batteryData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //battery drains 1% every (rate) seconds
+        bar.localScale = new Vector3((battery / 100), 1f);
+        if (rate == 1)//catch for decreasing 1 per frame
+        {
+            float t = Time.time - startTime;
+            battery = 100- (int)t;
+        }
+        else
+        {
+            float t = Time.time - startTime;
+            seconds = (int)t % (int)rate;
+            seconds++;
+
+            if (seconds == 1)
+                wait = false;
+
+            if (seconds == rate)
+            {
+                if (wait == false)
+                    battery--;
+                wait = true;
+            }
+        }
+        if (battery < 0)
+            battery = 0;
+
+        batteryData.text = "Battery: " + battery + "%";
+    }
+}
---
+++ Assets/Scripts/PseudoSensors/RNG.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RNG : MonoBehaviour
+{
+
+    public float input;
+    public float output;
+    public double bounds = 0.5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        output = input + NormRand(0, (float)bounds, 5);
+        output = Mathf.Round(output * 100.0f) * 0.01f;
+    }
+
+    public static float NormRand(float mu = 0, float sig = 1, float? z_max = null)
+    {
+        float u, v, S;
+
+        do
+        {
+            u = 2.0f * Random.value - 1.0f;
+            v = 2.0f * Random.value - 1.0f;
+            S = u * u + v * v;
+        }
+        while (S >= 1.0f);
+
+        float z = u * Mathf.Sqrt(-2.0f * Mathf.Log(S) / S);
+
+        if (z_max != null && Mathf.Abs(z) > z_max)
+        {
+            z = (float)z_max;
+        }
+
+        return z * sig + mu;
+    }
+
+    float getOutput()
+    {
+        return output;
+    }
+}
